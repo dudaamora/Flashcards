12e71fc038cc8ba3a79ecd3ee9466ab0f39da939
dudaamora <eduarda.amora@escola.pr.gov.br>
2025-07-01 16:54:16 -0300

    perguntas

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'CIÊNCIAS'
+    'como é feito a fotossintese?'
+    'pelo sol'
+)
+
+criaCartao(
+    'PROGRAMAÇÃO'
+    'o que é javaScript?'
+    'é uma linguagem de programação!'
+)
+
+criaCartao(
+    'GEOGRAFIA'
+    'qual é a captal do Brasil?'
+    'a captal do Brasil é Brasília'
+)
