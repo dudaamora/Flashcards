0fcbf6e2b30f18d7cac66e7b0ae5da2e3914de87
dudaamora <eduarda.amora@escola.pr.gov.br>
2025-07-03 15:16:27 -0300

    função

--- perguntas.js
+++ perguntas.js
@@ -1,18 +1,19 @@
 function criaCartao(categoria, pergunta, resposta){
-     console.log(categoria, pergunta, resposta);
+    console.log(categoria, pergunta, resposta);
     "CIÊNCIAS"
     "como é feito a fotossintese?"
     "pelo sol"
    
 }
 
-criaCartao(
+
     'PROGRAMAÇÃO'
     'o que é javaScript?'
     'é uma linguagem de programação!'
+
 )
 
-criaCartao(
+
     'GEOGRAFIA'
     'qual é a captal do Brasil?'
     'a captal do Brasil é Brasília'
